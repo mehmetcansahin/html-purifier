ba6b1851a53f846592eddcfe16655d6e30a1a207
mehmetcansahin <sahinmcan@gmail.com>
2020-02-20 21:36:46 +0300

    fragment tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
+use scraper::{Html, Selector};
+
+pub fn purifier() {
+    let html = r#"
+    <ul>
+        <li>Foo</li>
+        <li>Bar</li>
+        <li>Baz</li>
+    </ul>
+"#;
+
+    let fragment = Html::parse_fragment(html);
+    for node in fragment.tree {
+        
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
+        purifier();
         assert_eq!(2 + 2, 4);
     }
 }
