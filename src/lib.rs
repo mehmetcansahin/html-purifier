38a70f5aa23c9de66af452401c9786243fb1f4ba
mehmetcansahin <sahinmcan@gmail.com>
2020-02-22 00:42:53 +0300

    remove element

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,111 @@
 use lol_html::html_content::Element;
 use lol_html::{element, rewrite_str, RewriteStrSettings};
 
+pub struct AllowedElement {
+    pub name: String,
+    pub attribute: Vec<String>,
+}
+
+pub struct HTML {
+    pub allowed: Vec<AllowedElement>,
+}
+
+pub struct Settings {
+    pub default: HTML,
+}
+
+impl Default for Settings {
+    #[inline]
+    fn default() -> Self {
+        Settings {
+            default: HTML {
+                allowed: vec![
+                    AllowedElement {
+                        name: "div".to_string(),
+                        attribute: vec![],
+                    },
+                    AllowedElement {
+                        name: "b".to_string(),
+                        attribute: vec![],
+                    },
+                    AllowedElement {
+                        name: "strong".to_string(),
+                        attribute: vec![],
+                    },
+                    AllowedElement {
+                        name: "i".to_string(),
+                        attribute: vec![],
+                    },
+                    AllowedElement {
+                        name: "em".to_string(),
+                        attribute: vec![],
+                    },
+                    AllowedElement {
+                        name: "u".to_string(),
+                        attribute: vec![],
+                    },
+                    AllowedElement {
+                        name: "a[href|title]".to_string(),
+                        attribute: vec!["href".to_string(), "title".to_string()],
+                    },
+                    AllowedElement {
+                        name: "ul".to_string(),
+                        attribute: vec![],
+                    },
+                    AllowedElement {
+                        name: "ol".to_string(),
+                        attribute: vec![],
+                    },
+                    AllowedElement {
+                        name: "li".to_string(),
+                        attribute: vec![],
+                    },
+                    AllowedElement {
+                        name: "p".to_string(),
+                        attribute: vec!["style".to_string()],
+                    },
+                    AllowedElement {
+                        name: "br".to_string(),
+                        attribute: vec![],
+                    },
+                    AllowedElement {
+                        name: "span".to_string(),
+                        attribute: vec!["style".to_string()],
+                    },
+                    AllowedElement {
+                        name: "img".to_string(),
+                        attribute: vec![
+                            "width".to_string(),
+                            "height".to_string(),
+                            "alt".to_string(),
+                            "src".to_string(),
+                        ],
+                    },
+                ],
+            },
+        }
+    }
+}
+
 // https://docs.rs/lol_html/0.1.0/lol_html/html_content/struct.Element.html#method.remove
 pub fn purifier() {
+    let settings = Settings {
+        ..Settings::default()
+    };
+
     let input = r#"<div><span><!-- 42 --></span></div>"#;
 
-    let div_element_handler = |el: &mut Element| {
-        el.remove_and_keep_content();
+    let html_element_handler = |el: &mut Element| {
+        if settings
+            .default
+            .allowed
+            .iter()
+            .any(|x| x.name.eq(&el.tag_name()))
+        {
+            // attr check and remove
+        } else {
+            el.remove_and_keep_content();
+        }
         Ok(())
     };
 
@@ -14,8 +113,8 @@ pub fn purifier() {
         input,
         RewriteStrSettings {
             element_content_handlers: vec![
-                element!("div", div_element_handler),
-                element!("span", div_element_handler),
+                element!("div", html_element_handler),
+                element!("span", html_element_handler),
             ],
             ..RewriteStrSettings::default()
         },
