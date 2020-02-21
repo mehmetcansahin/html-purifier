ccdd8a80895870135f840a3fdde64f4dd57d8640
mehmetcansahin <sahinmcan@gmail.com>
2020-02-21 18:42:58 +0300

    lol_html

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
-use scraper::{Html, Selector};
-
+use lol_html::{element, rewrite_str, RewriteStrSettings};
+// https://docs.rs/lol_html/0.1.0/lol_html/html_content/struct.Element.html#method.remove
 pub fn purifier() {
-    let html = r#"
-    <ul>
-        <li>Foo</li>
-        <li>Bar</li>
-        <li>Baz</li>
-    </ul>
-"#;
+    let html = rewrite_str(
+        r#"<div><span><!-- 42 --></span></div>"#,
+        RewriteStrSettings {
+            element_content_handlers: vec![element!("div", |el| {
+                el.remove_and_keep_content();
 
-    let fragment = Html::parse_fragment(html);
-    for node in fragment.tree {
-
-    }
+                Ok(())
+            })],
+            ..RewriteStrSettings::default()
+        },
+    )
+    .unwrap();
+    println!("{}", html);
+    assert_eq!(html, r#"<span><!-- 42 --></span>"#);
 }
 
 #[cfg(test)]
